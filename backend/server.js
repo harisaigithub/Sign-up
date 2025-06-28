c5d56c25478a8520d9a78995a91e09799af776c3
HARI SAI PARASA <114827365+harisaigithub@users.noreply.github.com>
2025-06-28 22:38:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
-
+const dbUri = process.env.MONGO_URI;
 // MongoDB connection (replace with your actual connection string)
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
